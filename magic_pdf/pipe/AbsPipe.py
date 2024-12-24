67efac898ceb2e9d073a800a35d91ad84ae8622d
pangguosheng <pangguosheng@wps.com>
2024-12-24 09:29:34 +0800

    Revert "feat(classify)：add OCR mode switching based on char overlap detection"
    This reverts commit c3da5a0f74d219ddec51b74648363dcd37d8eb8a.

--- magic_pdf/pipe/AbsPipe.py
+++ magic_pdf/pipe/AbsPipe.py
@@ -76,7 +76,6 @@ class AbsPipe(ABC):
                     pdf_meta['imgs_per_page'],
                     pdf_meta['text_layout_per_page'],
                     pdf_meta['invalid_chars'],
-                    pdf_meta['char_overlap_counter']
                 )
                 if is_text_pdf:
                     return AbsPipe.PIP_TXT
